3a286a1648f1dab0e76a80a708bda3ada305fb37
K-Ramberg <kramberg0350@gmail.com>
2018-06-22 16:24:11 -0400

    initializing

---
+++ bog-express-two/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+mongoose.connect(process.env.MONGODB_URI)
+
+const connection = mongoose.connection
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully')
+})
+
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err)
+})
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/client/build'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
